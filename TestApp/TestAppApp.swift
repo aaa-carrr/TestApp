e5926cc14220d86f6cb6d6e7d73bfc6788ec5f2a
Artur Carneiro <96629817+aaa-carrr@users.noreply.github.com>
2024-11-07 20:49:02 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Artur Carneiro on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
