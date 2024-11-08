5a85bd3320df2744a8bf48ba9d8e06226907acfc
Artur Carneiro <96629817+aaa-carrr@users.noreply.github.com>
2024-11-08 05:38:57 +0100

    Update package configuration

--- TAPackage/Package.swift
+++ TAPackage/Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "TAPackage",
+    defaultLocalization: "en-US",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
